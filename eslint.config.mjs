2df2bfa44ad503aaea4bda63b0de59913d360913
Architha1323 <architha1323@gmail.com>
2026-07-28 20:43:14 +0530

    trigger vercel build for new env vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,7 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
-const eslintConfig = defineConfig([
+const eslintConfig = [
   ...nextVitals,
   ...nextTs,
   {
@@ -13,14 +12,14 @@ const eslintConfig = defineConfig([
       "react-hooks/set-state-in-effect": "off"
     }
   },
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+  {
+    ignores: [
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ]
+  }
+];
 
 export default eslintConfig;
